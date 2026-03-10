e052045de462ffa8c641b26d9d803227228a0e1c
Sebastian Romo <sromo1@umd.edu>
2026-03-09 23:10:37 -0400

    repo cleanup

--- README.md
+++
@@ -1,2 +0,0 @@
-# knowledge_companion
-Repository to house exercises to different knowledge sources.
--- coding_practice/test.py
+++
@@ -1,8 +0,0 @@
-import docx
-
-doc = docx.Document("doc.docx")
-text=""
-for paragraph in doc.paragraphs:
-    text += paragraph.text
-
-print(text)
